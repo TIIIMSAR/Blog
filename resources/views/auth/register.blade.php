65f883490f08feabadba6e0da78ac10f017198ad
Hoss MOHAMMADI <1386.hossein.mohammadi@gmail.com>
2024-06-03 06:29:18 -0700

    error-fa

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -8,20 +8,55 @@
         <div class="sign-page">
             <h1 class="sign-page__title">ثبت نام در وب‌سایت</h1>
 
-            @foreach ($errors->all() as $error)
-                <p>{{ $error }}</p>
-            @endforeach
-
             <form class="sign-page__form" action="{{ route('register.store') }}" method="POST">
                 @csrf            
-                    <input type="text" name="name" class="text text--right" placeholder="نام  و نام خانوادگی">
+                    <div>
+                        <input type="text" name="name" class="text text--right" placeholder="نام  و نام خانوادگی">
+                        @error('name')
+                            <p style="margin-bottom: 1rem;
+                                    color: crimson;
+                                    text-align: justify;
+                                    font-weight: 400;
+                                    font-family: Verdana, Geneva, Tahoma, sans-serif;">{{ $message }}</p>
+                        @enderror
+                    </div>
+
+                    <div>
                     <input type="text" name="mobile" class="text text--left" placeholder="شماره موبایل">
+                        @error('mobile')
+                            <p style="margin-bottom: 1rem;
+                            color: crimson;
+                            text-align: justify;
+                            font-weight: 400;
+                            font-family: Verdana, Geneva, Tahoma, sans-serif;">{{ $message }}</p>
+                        @enderror
+                    </div>
+                    <div>
                     <input type="text" name="email" class="text text--left" placeholder="ایمیل">
-                    <input type="password" name="password" class="text text--left" placeholder="رمز عبور">
-                    <input type="password" name="password_confirmation" class="text text--left" placeholder="تکرار رمز عبور">
-
-
+                        @error('email')
+                            <p style="margin-bottom: 1rem;
+                            color: crimson;
+                            text-align: justify;
+                            font-weight: 400;
+                            font-family: Verdana, Geneva, Tahoma, sans-serif;">{{ $message }}</p>
+                        @enderror
+                    </div>
+                    <div>
+                        <input type="password" name="password" class="text text--left" placeholder="رمز عبور">
+                        @error('password')
+                            <p style="margin-bottom: 1rem;
+                            color: crimson;
+                            text-align: justify;
+                            font-weight: 400;
+                            font-family: Verdana, Geneva, Tahoma, sans-serif;">{{ $message }}</p>
+                        @enderror
+                    </div>
+                    <div>
+                        <input type="password" name="password_confirmation" class="text text--left" placeholder="تکرار رمز عبور">
+                    </div>
                     <button class="btn btn--blue btn--shadow-blue width-100 mb-10" type="submit">ثبت نام</button>
+
+
                     <div class="sign-page__footer">
                         <span>در سایت عضوید ؟ </span>
                         <a href="sign-up.html" class="color--46b2f0">صفحه ورود</a>
